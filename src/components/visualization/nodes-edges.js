a38c777d1fa9a0f83b8d8995330dc2030c1a5ea2
njmorales <njmorales@ucsd.edu>
2025-02-09 23:49:22 -0800

    relief nodes

--- src/components/visualization/nodes-edges.js
+++ src/components/visualization/nodes-edges.js
@@ -44,7 +44,7 @@ export const reliefNodes = [
   {
     id: "2",
     type: "source",
-    data: { label: "Relief" },
+    data: { label: "Relief", numHandles: 2 },
     position: { x: 400, y: 300 },
     style: { backgroundColor: '#8b9df5', color: 'black' },
   },
@@ -63,8 +63,8 @@ export const reliefNodes = [
 ]
 
 export const reliefEdges = [
-  { id: "e22a", source: "2", target: "2a", sourceHandle: 'l', style: { strokeWidth: 3 }, animated: false },
-  { id: "e22b", source: "2", target: "2b", sourceHandle: 'r',style: { strokeWidth: 3 }, animated: false },
+  { id: "e22a", source: "2", target: "2a", sourceHandle: 'l1', style: { strokeWidth: 3 }, animated: false },
+  { id: "e22b", source: "2", target: "2b", sourceHandle: 'b1', style: { strokeWidth: 3 }, animated: false },
 ]
 
 //==============================================================================================================================================
