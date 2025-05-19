e35d100f8c0d433a2fb437a92692ac8ab5f111a2
Alden Lo <alden.ys.lo@gmail.com>
2025-05-19 10:45:09 -0700

    minor adjustments

--- src/components/network/NavButton.tsx
+++ src/components/network/NavButton.tsx
@@ -12,7 +12,7 @@ export default function NavButton({
   return (
     <button
       onClick={onClick}
-      className="bg-node-bg border-node-border flex cursor-pointer items-center gap-2 rounded-3xl border-4 px-5 py-3 text-lg font-semibold"
+      className="bg-node-bg border-node-border text-md flex cursor-pointer items-center gap-2 rounded-3xl border-4 px-4 py-2 font-semibold"
     >
       {iconPlacement == "left" && icon}
       {label}
